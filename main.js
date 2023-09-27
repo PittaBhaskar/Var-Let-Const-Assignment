9b36ccddba74d2c20f847601d75e1f8fa30401ac
PittaBhaskar <146064819+PittaBhaskar@users.noreply.github.com>
2023-09-27 18:09:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function changeimg(){
+    document.getElementById("img1").src="Plantation-2.jpg";
+    document.getElementById("img1").style.padding="20px";
+    document.getElementById("img1").style.borderColor="white";
+}
+ function changeimage(){
+    document.getElementById("img2").src="Nature-1.jpg";
+    document.getElementById("img2").style.padding="20px";
+    document.getElementById("img2").style.borderColor="white";
+}
+ function varExampleSample(){
+    if(true){
+        var trees="TREES";
+        console.log(trees);
+        fruits="FRUITS";
+        
+    }
+    console.log(fruits);
+}
+varExampleSample();
+
+
+function letExample(){
+    if(true){
+        let plant="PLANTS";
+        console.log(plant);
+        Soil="SOIL";
+        console.log(Soil);
+    }
+}
+letExample();
+
+function constExample(){
+    const grass="GRASS";
+    console.log(grass);
+    water="WATER";
+    console.log(water);
+    
+    }
+constExample();
+    
